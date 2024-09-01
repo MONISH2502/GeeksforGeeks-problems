f9c8ddf6932fc0a63fe61059988698b1564bcab9
MONISH. P <158838702+MONISH2502@users.noreply.github.com>
2024-09-01 19:27:40 +0530

    Update MissingNumber.java

--- Easy Problems/MissingNumber.java
+++ Easy Problems/MissingNumber.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-class GFG {
+class Missingnumber {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

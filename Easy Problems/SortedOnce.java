622c64ce358ae81b3d8e18d04f10d55ed2f8e982
MONISH. P <158838702+MONISH2502@users.noreply.github.com>
2024-09-01 12:47:43 +0530

    Create SortedOnce.java
    a new coding solution was added

---
+++ Easy Problems/SortedOnce.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class GFG 
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while (t-- > 0) 
+        {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0;i < n;i++)
+            {
+                arr[i] = sc.nextInt();
+            }
+            
+            Solution sol = new Solution();
+            System.out.println(sol.findOnce(arr, n));
+        }
+    }
+}
+
+class Solution 
+{
+    int findOnce(int arr[], int n)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i : arr)
+        {
+            if(list.contains(i))
+              {
+                  list.remove((Integer)i);
+                  continue;
+              }
+              
+            list.add(i);
+        }
+        
+    return list.get(0);
+    }
+}
